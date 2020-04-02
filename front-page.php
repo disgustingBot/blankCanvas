973685e044b7a9c8150f24014468c82efea9c37e
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2020-04-02 18:16:06 +0100

    front-page.php

---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+
+<h1>front-page.php</h1>
+
+
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+  
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -14,5 +14,5 @@
 	</view>
 
   <header class="header" id="header">
-    
+
   </header>
