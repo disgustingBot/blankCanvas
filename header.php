bebf438e1b8da0d569cd5e69bbe78e22dfdf773d
Tomas <tomas.mparra@gmail.com>
2020-10-28 09:25:11 +0100

    Improvement
    added posibility to change input type when adding meta data to taxonomies for custom post type

--- header.php
+++ header.php
@@ -1,10 +1,14 @@
+<?php
+$site = '6LcRuNAUAAAAADtamJW75fYf8YtNHceSngjKsf-B';
+$scrt = '6LcRuNAUAAAAALBu7Ymh0yxmTXTJmP0rsnkjGyj0';
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  
+
   <?php wp_head(); ?>
   <style>
     :root{
