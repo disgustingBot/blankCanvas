4a89d99f2a1151612493d580e1da422c31d510ee
Sofia <molinerozadkiel@gmail.com>
2021-08-13 16:05:06 +0200

    select box

--- inc/select_box.php
+++ inc/select_box.php
@@ -5,7 +5,13 @@
 This function generates a selectBox object
 
 PARAMETROS:
-$name => el nombre visible o "label" del select
+$config => un vector que da configuraciones generales al select del tipo:
+array(
+	'label' => 'Select',
+	'class' => 'my_class or_classes',
+	'slug' => 'select',
+	'empty' => 'Empty'
+)
 $options => un vector del tipo:
 array(
 	[0] => array(
@@ -18,63 +24,75 @@ array(
 		'name' => 'option_1_name'
 	)
 )
-$empty_label => el nombre visible de la opcion de vaciar el select
-$slug => el nombre invisible del select (para CSS) se concatena a selectBox, ejemplo:
-$slug = 'MiSelect' resulta en la clase -> 'selectBoxMiSelect'
+$config['empty'] => el nombre visible de la opcion de vaciar el select
+$config['slug'] => el nombre invisible del select (para CSS) se concatena a selectBox, ejemplo:
+$config['slug'] = 'MiSelect' resulta en la clase -> 'selectBoxMiSelect'
 */
-function selectBox($name, $options = array(), $empty_label = 'Vaciar', $slug = false){
-	if(!$slug){ $slug = sanitize_title($name); }
+function selectBox($config, $options = array()){
+	if(!isset($config['slug'])){ $config['slug'] = sanitize_title($config['label']); }
 	// var_dump($options);
-	$selected = array('name'=>'','selected'=>False);
+
+	$config_default = array(
+		'label' => 'Select',
+		'class' => 'my_class or_classes',
+		'empty' => 'Empty',
+		'slug'  => 'select',
+	);
+
+	// check for selected items on the option list
+	$start = array('name'=>'','selected'=>False);
 	$selected = array_reduce($options,function($accu, $opt){
-		if ($accu != False) return $accu;
 		if (isset($opt['selected']) and $opt['selected'] == True) return $opt;
-		return False;},
-	False);
+		return $accu;
+	},$start);
 
 	// var_dump($selected);
 	?>
-	<div class="SelectBox <?php if ($selected['selected']) {echo 'alt';} ?> selectBox<?= $slug; ?>" tabindex="1" id="selectBox<?= $slug; ?>">
-		<div class="selectBoxButton" onclick="altClassFromSelector('focus', '#selectBox<?= $slug; ?>')">
-			<p class="selectBoxPlaceholder"><?= $name; ?></p>
-			<p class="selectBoxCurrent" id="selectBoxCurrent<?= $slug; ?>"><?php if(isset($selected['name'])) echo $selected['name']; ?></p>
+	<div class="SelectBox <?php if ( $selected['selected'] ){echo 'alt';} ?> <?= $config['class']; ?>" tabindex="1" id="selectBox<?= $config['slug']; ?>">
+		<div class="selectBoxButton" onclick="altClassFromSelector('focus', '#selectBox<?= $config['slug']; ?>')">
+			<svg class="select_box_icon dropdown_icon" width="32" height="16" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M31.7481 0.701755L31.2388 0.232817C30.9017 -0.0776058 30.3566 -0.0776058 30.0194 0.232817L16.004 13.1451L1.98145 0.232817C1.64434 -0.0776058 1.09921 -0.0776058 0.762097 0.232817L0.252837 0.701755C-0.0842789 1.01218 -0.0842789 1.51414 0.252837 1.82456L15.3872 15.7672C15.7243 16.0776 16.2694 16.0776 16.6065 15.7672L31.7409 1.82456C32.0852 1.51414 32.0852 1.01218 31.7481 0.701755Z" fill="currentColor"/>
+			</svg>
+			<p class="selectBoxPlaceholder"><?= $config['label']; ?></p>
+			<p class="selectBoxCurrent" id="selectBoxCurrent<?= $config['slug']; ?>"><?php if(isset($selected['name'])) echo $selected['name']; ?></p>
 		</div>
 		<div class="selectBoxList focus">
-			<label for="nul<?= $slug; ?>" class="selectBoxOption" id="selectBoxOptionNul"><?= $empty_label; ?>
+			<label for="nul_<?= $config['slug']; ?>" class="selectBoxOption" id="selectBoxOptionNul">
 				<input
 					class="selectBoxInput"
-					id="nul<?= $slug; ?>"
+					id="nul_<?= $config['slug']; ?>"
 					type="radio"
-					name="<?= $slug; ?>"
-					onclick="selectBoxControler('','#selectBox<?= $slug; ?>','#selectBoxCurrent<?= $slug; ?>')"
+					name="<?= $config['slug']; ?>"
+					onclick="selectBoxControler('','#selectBox<?= $config['slug']; ?>','#selectBoxCurrent<?= $config['slug']; ?>')"
 					value="0"
-					<?php if(!isset($_GET[$slug])){ ?>
+					<?php if(!$selected['selected']){ ?>
 						checked
 					<?php } ?>
 				>
 				<!-- <span class="checkmark"></span> -->
-				<p class="colrOptP"></p>
+				<p class="colrOptP"><?= $config['empty']; ?></p>
 			</label>
 
 
 			<?php foreach ($options as $option) {
+				if(!isset($option['value'])){$option['value'] = $option['slug'];}
 				$option['name'] = preg_replace('/\s+/', ' ', trim($option['name'])); ?>
 
-				<label for="<?= $slug; ?>_<?= $option['slug']; ?>" class="selectBoxOption">
+				<label for="<?= $config['slug']; ?>_<?= $option['slug']; ?>" class="selectBoxOption">
 					<input
 						class="selectBoxInput <?= $option['slug']; ?>"
 						type="radio"
-						id="<?= $slug; ?>_<?= $option['slug']; ?>"
-						name="<?= $slug; ?>"
-						onclick="selectBoxControler('<?= $option['name']; ?>', '#selectBox<?= $slug; ?>', '#selectBoxCurrent<?= $slug; ?>')"
-						value="<?= $option['slug']; ?>"
+						id="<?= $config['slug']; ?>_<?= $option['slug']; ?>"
+						name="<?= $config['slug']; ?>"
+						onclick="selectBoxControler('<?= $option['name']; ?>', '#selectBox<?= $config['slug']; ?>', '#selectBoxCurrent<?= $config['slug']; ?>')"
+						value="<?= $option['value']; ?>"
 						<?php if(isset($option['selected']) && $option['selected'] == True){ ?>
 							checked
 						<?php } ?>
 						<?php
 						if (isset($option['data'])) {
 							foreach ($option['data'] as $key => $value) {
-								echo "$key='$value'";
+								echo "data-$key='$value'";
 							}
 						}
 						?>
